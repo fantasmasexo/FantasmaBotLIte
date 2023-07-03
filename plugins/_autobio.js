b34f076829a0bfcdae8de6177a92cdb505048a53
fantasmasexo <138138974+fantasmasexo@users.noreply.github.com>
2023-07-03 14:52:07 -0600

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -9,7 +9,7 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 2000) }) * 1000}
 
 let uptime = clockString(_uptime)
-let bio = `${global.packname} ⁝⁝ ✅ ${uptime} ⌛ ⁝⁝ ${lenguajeGB.lenguaje() == 'es' ? '#estado #menu #creador' : '#status #menu # #groupsfa #owner'} 💻`
+let bio = `${global.packname} ✅ ${uptime} ⌛ ⁝⁝ ${lenguajeGB.lenguaje() == 'es' ? '#estado #menu #creador' : '#status #menu # #groupsfa #owner'} 💻`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 }
